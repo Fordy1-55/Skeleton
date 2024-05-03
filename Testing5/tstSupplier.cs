62bfc72f5531694681412e4646eab7f5c4b064f3
P2727187 <P2727187@my365.dmu.ac.uk>
2024-05-03 10:53:59 +0100

    Added Testing to Supplier

---
+++ Testing5/tstSupplier.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstSupplier
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsSupplier AnSupplier = new clsSupplier();
+            Assert.IsNotNull(AnSupplier);
+        }
+    }
+}
