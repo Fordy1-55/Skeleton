6616f2036f2a04af2ca91af74b41ab164bff8c49
h0lly0cr <hollydolly76@me.com>
2024-05-03 10:35:49 +0100

    thius is my testing for staff class

---
+++ Testing2/clsStaff.cs
@@ -0,0 +1,6 @@
+﻿namespace Testing2
+{
+    public class clsStaff
+    {
+    }
+}
---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create new instance of class
+            clsStaff Staff = new clsStaff();
+            //test to see if it exists
+            Assert.IsNotNull(Staff);
+        }
+    }
+}
