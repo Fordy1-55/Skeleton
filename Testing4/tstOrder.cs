5facf3ac2f549d1fc051ffb1863e4450b065495a
Fordy1-55 <cam.fordy@gmail.com>
2024-05-03 09:40:17 +0100

    Added testing for address class

---
+++ Testing4/tstOrder.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //test to see that it exists
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
