0c14a0ac9304241352d3099762e2e1d634be3561
P2727187 <P2727187@my365.dmu.ac.uk>
2024-05-08 15:13:08 +0100

    Added properties and testing for those properties to Supplier

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int SupplierID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
     }
 }
--- Testing5/tstSupplier.cs
+++ Testing5/tstSupplier.cs
@@ -13,5 +13,68 @@ namespace Testing5
             clsSupplier AnSupplier = new clsSupplier();
             Assert.IsNotNull(AnSupplier);
         }
+
+        [TestMethod]
+        public void AvailableSupplierOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            Boolean TestData = true;
+            ASupplier.Active = TestData;
+            Assert.AreEqual(ASupplier.Active, TestData);
+        }
+
+        [TestMethod]
+        public void DateAddedSupplierOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            DateTime TestData = DateTime.Now.Date;
+            ASupplier.DateAdded = TestData;
+            Assert.AreEqual(ASupplier.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void SupplierIDOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            Int32 TestData = 1;
+            ASupplier.SupplierID = TestData;
+            Assert.AreEqual(ASupplier.SupplierID, TestData);
+        }
+
+        [TestMethod]
+        public void NameSupplierOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            string TestData = "Hats";
+            ASupplier.Name = TestData;
+            Assert.AreEqual(ASupplier.Name, TestData);
+        }
+
+        [TestMethod]
+        public void ContactEmailSupplierOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            string TestData = "he got an email";
+            ASupplier.Email = TestData;
+            Assert.AreEqual(ASupplier.Email, TestData);
+        }
+
+        [TestMethod]
+        public void ContactPhoneSupplierOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            string TestData = "he got a phone";
+            ASupplier.Phone = TestData;
+            Assert.AreEqual(ASupplier.Phone, TestData);
+        }
+
+        [TestMethod]
+        public void CitySupplierOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            string TestData = "city";
+            ASupplier.City = TestData;
+            Assert.AreEqual(ASupplier.City, TestData);
+        }
     }
 }
