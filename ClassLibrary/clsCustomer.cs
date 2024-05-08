e6c88b5643f71af18a5eaf5f03483402a9c9cb06
MishaVaria <mishavaria05@hotmail.com>
2024-05-08 15:57:48 +0100

    created test properties for the classes

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,9 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
-        public clsCustomer()
-        {
-        }
+        public bool SubscriptionProperty { get; set; }
+        public DateTime DateOfBirthProperty { get; set; }
+        public string CustomerNameProperty { get; set; }
+        public string CustomerEmailProperty { get; set; }
+        public string CustomerAddressProperty { get; set; }
+        public string CustomerTelphoneNoProperty { get; set; }
     }
 }
