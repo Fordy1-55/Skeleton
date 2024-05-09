f1c6a43965793310cda06af217f8870904f5e762
Fordy1-55 <cam.fordy@gmail.com>
2024-05-09 10:56:32 +0100

    Added a simple Presentation Layer

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,11 +4,60 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
+
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox3_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox4_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //capture the order Description
+        AnOrder.OrderDescription = txtOrderDescription.Text;
+        //capture the orderID
+        AnOrder.OrderId = Convert.ToInt32(txtOrderId.Text);
+        AnOrder.OrderPrice = Convert.ToDouble(txtPrice.Text);
+        AnOrder.DateOrdered = Convert.ToDateTime(txtDateOrdered.Text);
+        AnOrder.DeliveryInstructions = txtDeliveryInstructions.Text;
+        AnOrder.ReturnAddress = txtReturnAddress.Text;
+        AnOrder.OverseasDelivery = chkOverseas.Checked;
+        //store the description in the session object
+        Session["AnOrder"] = AnOrder;
+        //navigate to the view page
+        Response.Redirect("OrderViewer.aspx");
+
+
 
     }
 }
--- AdminSystem/OrderViewer.aspx.cs
+++ AdminSystem/OrderViewer.aspx.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //get the data from the session object
+        AnOrder = (clsOrder)Session["AnOrder"];
+        //display the order description for this entry
+        Response.Write(AnOrder.OrderId);
+        Response.Write(AnOrder.OrderDescription);
+        Response.Write(AnOrder.DateOrdered);
+        Response.Write(AnOrder.OrderPrice);
+        Response.Write(AnOrder.ReturnAddress);
+        Response.Write(AnOrder.DeliveryInstructions);
+        Response.Write(AnOrder.OverseasDelivery);
+
+
+
+
+
 
     }
 }
