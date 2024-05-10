b2d6f5d8f3144d764f434e1153da6877f62e426b
Fordy1-55 <cam.fordy@gmail.com>
2024-05-10 11:46:24 +0100

    Practical 9A completed. Added the find method and created working tests.

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -23,62 +23,108 @@ namespace ClassLibrary
             }
         }
 
+        //private data member for the order description property
         private string mOrderDescription;
+        //orderId public property
         public string OrderDescription
         {
             get
             {
+                //this line of code sends data out of the property
                 return mOrderDescription;
             }
             set
             {
+                //this line of code allows data into the property
                 mOrderDescription = value;
             }
         }
 
+        //private data member for the order price property
         private double mOrderPrice;
+        //orderId public property
         public double OrderPrice
         {
             get
             {
+                //this line of code sends data out of the property
                 return mOrderPrice;
             }
             set
             {
+                //this line of code allows data into the property
                 mOrderPrice = value;
             }
         }
 
+        //private data member for the overseas delivery property
         private bool mOverseasDelivery;
+        //orderId public property
         public bool OverseasDelivery
         {
             get
             {
+                //this line of code sends data out of the property
                 return mOverseasDelivery;
             }
             set
             {
+                //this line of code allows data into the property
                 mOverseasDelivery = value;
             }
         }
 
+        //private data member for the date ordered property
         private DateTime mDateOrdered;
+        //orderId public property
         public DateTime DateOrdered
         {
             get
             {
+                //this line of code sends data out of the property
                 return mDateOrdered;
             }
             set
             {
+                //this line of code allows data into the property
                 mDateOrdered = value;
             }
         }
 
+        //private data member for the return address property
         private string mReturnAddress;
+        //orderId public property
         public string ReturnAddress
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mReturnAddress;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mReturnAddress = value;
+            }
+        }
 
-        public string DeliveryInstructions { get; set; }
+        //private data member for the delivery instructions property
+        private string mDeliveryInstructions;
+        //orderId public property
+        public string DeliveryInstructions
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mDeliveryInstructions;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mDeliveryInstructions = value;
+            }
+        }
+
 
         public bool Find(int OrderId)
         {
@@ -89,6 +135,7 @@ namespace ClassLibrary
             mOrderPrice = Convert.ToDouble("1.99");
             mOverseasDelivery = true;
             mReturnAddress = "Test Address";
+            mDeliveryInstructions = "Test Instructions";
             //always return true
             return true;
         }
