4c0667148939204a75bb2cddfa87efbf4527711f
asolomon0421 <P2729497@my365.dmu.ac.uk>
2024-05-10 13:33:43 +0100

    web forms stock

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -11,4 +12,54 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void txtProductTitle_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtProductDescription_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtProductColour_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtProductId_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void txtProductPrice_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        clsStock AnStock = new clsStock();
+        //capture the Product Title, Product Description, Product Colour, Product Id, Product Price, Date Added, Available check box
+        AnStock.ProductTitle = txtProductTitle.Text;
+
+        AnStock.ProductDescription = txtProductDescription.Text;
+
+        AnStock.ProductColour = txtProductColour.Text;
+ 
+        AnStock.ProductId = Convert.ToInt32(txtProductId.Text);
+ 
+        AnStock.ProductPrice = Convert.ToDouble(txtProductPrice.Text);
+
+        AnStock.DateAdded = Convert.ToDateTime(DateTime.Now);
+
+        AnStock.Available = ChkAvaible.Checked;  
+        //store the data in the session object
+        Session["AnStock"] = AnStock;
+        //display the prodcut id for this entry
+        Response.Redirect("StockViewer.aspx");
+    }
+
+
+    
 }
