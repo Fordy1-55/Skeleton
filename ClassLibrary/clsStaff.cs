0f3a8906d95f40a0df1a3bb410e3336ddb5b5e62
h0lly0cr <hollydolly76@me.com>
2024-05-20 21:04:18 +0100

    practical 10 done

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -148,8 +148,10 @@ namespace ClassLibrary
             
         }
 
-        public string Valid(string Name, string role, string shiftType, string startDate)
+        public string Valid(string Name, string Role, string ShiftType, string StartDate)
         {
+            //create temp variable to store date values
+            DateTime DateTemp;
             //create string variable to store the error
             String Error = "";
             //if name is blank
@@ -164,9 +166,68 @@ namespace ClassLibrary
                 //record error
                 Error = Error + "Name may not be more than 35 Characters";
             }
-            //Return any error message
+
+            //create an instance of DateTime to compare with DateTemp
+            //in the if statement
+            DateTime DateComp = DateTime.Now.Date;
+
+            try
+            {
+                //copy the StartDate value to the DateTemp variable
+                DateTemp = Convert.ToDateTime(StartDate);
+
+                if (DateTemp < DateComp)
+                {
+                    //record the error
+                    Error = Error + "Date cannot be in the past";
+
+                }
+                //check to see if date is greater than todays date
+                if (DateTemp > DateComp)
+                {
+                    //record the error
+                    Error = Error + "Date cannot be in the future";
+                }
+            }
+            catch 
+            {
+                //record the error
+                Error = Error + "The date was not a valid date : ";
+            }
+            //is role blank
+            if(Role.Length == 0) 
+            {
+                //record error
+                Error = Error + "Role may not be blank";
+            }
+            //Role too long
+            if(Role.Length > 30)
+            {
+                //record error
+                Error = Error + "Role may not be more than 30 characters";
+
+            }
+            //is shiftype blank?
+            if(ShiftType.Length == 0)
+            {
+                //record error
+                Error = Error + "Shift type may not be blank";
+            }
+            //is shiftype more than 25 characters
+            if (ShiftType.Length > 25)
+            {
+                //record the error
+                Error = Error + "Shift type must be no more than 25 Characters";
+            }
+            //return any error messages
             return Error;
+
+            
         }
+
+
+       
+        
     }
 
     
