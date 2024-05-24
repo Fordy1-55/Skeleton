dd1a07b3cc8229396c0ed4cb2396d35beee3d988
asolomon0421 <P2729497@my365.dmu.ac.uk>
2024-05-24 21:34:26 +0100

    latest work

--- AdminSystem/StockList.aspx
+++ AdminSystem/StockList.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:ListBox ID="lstStockList" runat="server" style="z-index: 1; left: 10px; top: 34px; position: absolute; height: 568px; width: 527px"></asp:ListBox>
     </form>
 </body>
 </html>
---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+
+    public class clsStockCollection
+    {
+
+        //private data member for the list
+        List<clsStock> mStockList = new List<clsStock>();
+
+        public List<clsStock> StockList
+        { get
+            {
+                //return the private data
+                return mStockList;
+            }
+            set
+            {
+                //Set the private data
+                mStockList = value;
+            }
+        }
+
+        public int StockCount
+        {
+            get
+            {
+                //return the count list
+                return mStockList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsStock ThisStock { get; set; }
+
+
+
+        //constructor for the class
+        public clsStockCollection()
+        {
+            //variable for the index
+            Int32 Index = 0;
+            //variable for index
+            Int32 RecordCount = 0;
+            //object for the data
+            clsDataConnection DB = new clsDataConnection();
+            //excute the stored procedure
+            DB.Execute("sproc_tblProduct_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount) 
+            {
+                //create a blank stock
+                clsStock AnStock = new clsStock();
+                //read i the fields for the currecnt reocrd
+                AnStock.ProductId = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductId"]);
+                AnStock.ProductDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["ProductDate"]);
+                AnStock.ProductDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["ProductDate"]);
+                AnStock.ProductAvailable = Convert.ToBoolean(DB.DataTable.Rows[Index]["ProductAvailable"]);
+                AnStock.ProductPrice = Convert.ToDecimal(DB.DataTable.Rows[Index]["ProductPrice"]);
+                AnStock.ProductDescription = Convert.ToString(DB.DataTable.Rows[Index]["ProductDescription"]);
+                AnStock.ProductTitle = Convert.ToString(DB.DataTable.Rows[Index]["ProductTitle"]);
+                AnStock.ProductColour = Convert.ToString(DB.DataTable.Rows[Index]["ProductColour"]);
+                //add the record to the private data member
+                mStockList.Add(AnStock);
+                //point at the next record
+                Index++;
+            }
+
+            //create the item of the test data
+            clsStock TestItem = new clsStock();
+            //set its properties
+            TestItem.ProductAvailable = true;
+            TestItem.ProductId = 1;
+            TestItem.ProductDate = DateTime.Now;
+            TestItem.ProductPrice = 6.25M;
+            TestItem.ProductDescription = "Test Description";
+            TestItem.ProductTitle = "Test Title";
+            TestItem.ProductColour = "Test Colour";
+            //add the test item to the test list
+            mStockList.Add(TestItem);
+            //re intialise the object for some new data
+            TestItem = new clsStock();
+            //set its properties
+            TestItem.ProductAvailable = true;
+            TestItem.ProductId = 2;
+            TestItem.ProductDate = DateTime.Now;
+            TestItem.ProductPrice = 6.50M;
+            TestItem.ProductDescription = "Test Didderent Description";
+            TestItem.ProductTitle = "Test Another Title";
+            TestItem.ProductColour = "Test Diiferent Colour";
+            //add the item to the test list
+            mStockList.Add(TestItem);
+        }
+
+    }
+}
